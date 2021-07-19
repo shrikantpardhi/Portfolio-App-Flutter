dfceedf03459d34eb005e0b5f0a8ec6819dc9580
Shrikant Pardhi <dynast.dev@gmail.com>
2021-07-19 14:42:03 +0530

    communication links added

--- lib/widgets/body.dart
+++ lib/widgets/body.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:mailto/mailto.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'contact_button.dart';
 
 class Body extends StatelessWidget {
-  const Body({Key? key}) : super(key: key);
-
+  Body({Key? key}) : super(key: key);
+  final List projectsList = [
+    {
+      'title': 'Building a Cat',
+      'subtitle': 'Great client',
+      'image': 'https://picsum.photos/id/100/400/300'
+    },
+    {
+      'title': 'Flutter 2.0 Course',
+      'subtitle': 'The best of the best!',
+      'image': 'https://picsum.photos/id/100/400/300'
+    },
+    {
+      'title': 'Connekto',
+      'subtitle': 'A Flutter app for nerds',
+      'image': 'https://picsum.photos/id/1014/400/300'
+    },
+    {
+      'title': 'Been There',
+      'subtitle': 'Save places you\'ve visited',
+      'image': 'https://picsum.photos/id/3/400/300'
+    },
+    {
+      'title': 'Bengo',
+      'subtitle': 'Flutter email app',
+      'image': 'https://picsum.photos/id/1025/400/300'
+    }
+  ];
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -24,29 +52,32 @@ class Body extends StatelessWidget {
                       Opacity(opacity: 0.4, child: Image.asset('headshot.png')),
                       Align(
                         alignment: Alignment.topCenter,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              'I\'m Shrikant Pardhi \nA Software Developer',
-                              style: TextStyle(
-                                fontSize: 44.5,
-                                fontWeight: FontWeight.w500,
-                                color: Colors.blueGrey,
+                        child: Expanded(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'I\'m Shrikant Pardhi \nA Software Developer',
+                                style: TextStyle(
+                                  fontSize: 44.5,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.blueGrey,
+                                ),
                               ),
-                            ),
-                            Align(
-                              alignment: Alignment.centerLeft,
-                              child: Padding(
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 80, vertical: 60),
-                                child: ContactButton(
+                              Align(
+                                alignment: Alignment.centerLeft,
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 80, vertical: 60),
+                                  child: ContactButton(
                                     buttonText: 'Drop a Line',
                                     icon: Icon(Icons.mail_outline),
-                                    onPressed: () {}),
-                              ),
-                            )
-                          ],
+                                    onPressed: () => launchMailto(),
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
                         ),
                       ),
                     ],
@@ -57,11 +88,72 @@ class Body extends StatelessWidget {
           ),
         ),
         Expanded(
-            flex: 2,
-            child: Container(
-              color: Colors.white,
-            ))
+          flex: 2,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 100,
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  'My Projects',
+                  style: TextStyle(
+                    color: Colors.black45,
+                    fontSize: 23,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: projectsList.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      child: Column(
+                        children: [
+                          Card(
+                            elevation: 3,
+                            child: Column(
+                              children: [
+                                ListTile(
+                                  leading: Icon(Icons.work),
+                                  title: Text(projectsList[index]['title']),
+                                  subtitle:
+                                      Text(projectsList[index]['subtitle']),
+                                ),
+                                Container(
+                                  child: Image.network(
+                                      projectsList[index]['image']),
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(width: 50)
       ],
     );
+  }
+
+  launchMailto() async {
+    final mailtoLink = Mailto(
+      to: ['dynast.dev@gmail.com'],
+      // cc: ['cc1@example.com', 'cc2@example.com'],
+      subject: 'mailto example subject',
+      body: 'mailto example body',
+    );
+    // Convert the Mailto instance into a string.
+    // Use either Dart's string interpolation
+    // or the toString() method.
+    await launch('$mailtoLink');
   }
 }
--- lib/widgets/buttonrow.dart
+++ lib/widgets/buttonrow.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ButtonRow extends StatelessWidget {
   const ButtonRow({Key? key}) : super(key: key);
@@ -14,17 +15,17 @@ class ButtonRow extends StatelessWidget {
           child: Row(
             children: [
               TextButton.icon(
-                  onPressed: () {},
+                  onPressed: () => _launchURL('https://www.facebook.com/'),
                   icon: Image.asset('images/social/facebook.png',
                       width: 15, height: 15),
                   label: Text('')),
               TextButton.icon(
-                  onPressed: () {},
+                  onPressed: () => _launchURL('https://www.instagram.com/'),
                   icon: Image.asset('images/social/instagram.png',
                       width: 15, height: 15),
                   label: Text('')),
               TextButton.icon(
-                  onPressed: () {},
+                  onPressed: () => _launchURL('https://www.twitter.com/'),
                   icon: Image.asset('images/social/twitter.png',
                       width: 15, height: 15),
                   label: Text('')),
@@ -34,4 +35,7 @@ class ButtonRow extends StatelessWidget {
       ],
     );
   }
+
+  void _launchURL(String url) async =>
+      await canLaunch(url) ? await launch(url) : throw 'Could not launch $url';
 }
